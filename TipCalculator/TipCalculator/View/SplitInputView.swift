7db8e2e2b9b4112b68c5b3782050508e3f6fbadd
Sunil Maurya <sunil@shearshare.com>
2024-04-03 22:51:32 +0530

    added Split Bill View

--- TipCalculator/TipCalculator/View/SplitInputView.swift
+++ TipCalculator/TipCalculator/View/SplitInputView.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class SplitInputView: UIView {
 
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(title: "Split", subTitle: "the total")
+        return view
+    }()
+
+    private lazy var decrementButton: UIButton = {
+        buildButton(text: "-", corners: [.layerMinXMaxYCorner, .layerMinXMinYCorner])
+    }()
+    
+    private lazy var incrementButton: UIButton = {
+        buildButton(text: "+", corners: [.layerMaxXMinYCorner, .layerMaxXMaxYCorner])
+    }()
+
+    private lazy var quantityLabel: UILabel = {
+        let label = LabelFactory.build(text: "1", font: ThemeFont.bold(of: 20), backgroundColor: .white)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [decrementButton, quantityLabel, incrementButton])
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        return stackView
+    }()
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,7 +45,34 @@ class SplitInputView: UIView {
     }
 
     private func layout() {
-        backgroundColor = .cyan
+        [headerView, stackView].forEach(addSubview(_:))
+        
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        [incrementButton, decrementButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(stackView.snp.centerY)
+            make.trailing.equalTo(stackView.snp.leading).offset(-24)
+            make.width.equalTo(68)
+        }
+    }
+    
+    private func buildButton(text: String, corners: CACornerMask) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(of: 20)
+        button.backgroundColor = ThemeColor.primary
+        button.tintColor = .white
+        button.addRoundedCorners(corners: corners, radius: 8.0)
+        return button
     }
 }
 
