83065a3117baefe3c74a701364c2c70fef8de5ab
Sunil Maurya <sunil@shearshare.com>
2024-04-03 22:26:57 +0530

    added Tooltip view

--- TipCalculator/TipCalculator/View/TipInputView.swift
+++ TipCalculator/TipCalculator/View/TipInputView.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class TipInputView: UIView {
 
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(title: "Choose", subTitle: "your tip")
+        return view
+    }()
+    
+    private lazy var tenPercentTipButton: UIButton = {
+        buildTipButton(tip: .tenPercent)
+    }()
+    
+    private lazy var fifteenPercentTipButton: UIButton = {
+        buildTipButton(tip: .fifteenPercent)
+    }()
+    
+    private lazy var twentyPercentTipButton: UIButton = {
+        buildTipButton(tip: .twentyPercent)
+    }()
+    
+    private lazy var customTipButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("custom tip", for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(of: 20)
+        button.backgroundColor = ThemeColor.primary
+        button.tintColor = .white
+        button.addCornerRadius(radius: 8)
+        return button
+    }()
+
+    private lazy var hStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [tenPercentTipButton, fifteenPercentTipButton, twentyPercentTipButton])
+        view.axis = .horizontal
+        view.distribution = .fillEqually
+        view.spacing = 16
+        return view
+    }()
+
+    private lazy var buttonStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [hStackView, customTipButton])
+        view.axis = .vertical
+        view.distribution = .fillEqually
+        view.spacing = 16
+        return view
+    }()
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -19,6 +63,48 @@ class TipInputView: UIView {
     }
 
     private func layout() {
-        backgroundColor = .green
+        [headerView, buttonStackView].forEach(self.addSubview(_:))
+        buttonStackView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+        }
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalTo(buttonStackView.snp.leading).offset(-24)
+            make.width.equalTo(68)
+            make.centerY.equalTo(hStackView.snp.centerY)
+        }
+    }
+    
+    private func buildTipButton(tip: Tip) -> UIButton {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = ThemeColor.primary
+        button.addCornerRadius(radius: 8.0)
+        let text = NSMutableAttributedString(string: tip.stringValue, attributes:[.font: ThemeFont.bold(of: 20), .foregroundColor: UIColor.white])
+        text.addAttributes([.font: ThemeFont.demiBold(of: 14)], range: NSMakeRange(2, 1))
+        button.setAttributedTitle(text, for: .normal)
+        return button
+    }
+}
+
+enum Tip {
+    case none
+    case tenPercent
+    case fifteenPercent
+    case twentyPercent
+    case custom(value: Int)
+    
+    var stringValue: String {
+        switch self {
+        case .none:
+            return ""
+        case .tenPercent:
+            return "10%"
+        case .fifteenPercent:
+            return "15%"
+        case .twentyPercent:
+            return "20%"
+        case .custom(let value):
+            return String(value)
+        }
     }
 }
